ea108c96e47d42a49a28b7463f2047ad883db197
Elliot Lee <eal001@ucsd.edu>
2020-12-23 21:11:16 -0800

    Added tableview of exercises to dayexerciseviewcontroller

--- Workout App/SingleDayViewController.swift
+++ Workout App/SingleDayViewController.swift
@@ -7,21 +7,41 @@
 
 import UIKit
 
-class SingleDayViewController: UIViewController {
+class SingleDayViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var day : Day?
     var exercises = [Exercise]()
     
     @IBOutlet weak var day_name_label: UILabel!
+    @IBOutlet weak var exercise_table: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //print("loaded")
+        exercise_table.delegate = self
+        exercise_table.dataSource = self
         day_name_label.text = day?.get_day_str()
-        // Do any additional setup after loading the view.
+        
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Exercices"
+    }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return exercises.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "proto_cell", for: indexPath)
+        cell.textLabel?.text = exercises[indexPath.row].name
+        return cell
+    }
+    
     /*
     // MARK: - Navigation
 
