600389f6891a2992c160222cbade529b1896f2d8
Elliot Lee <eal001@ucsd.edu>
2020-12-27 17:51:47 -0800

    Created a functional Progress Chart for Exercises in a cycle

---
+++ Workout App/ChartCell.swift
@@ -0,0 +1,136 @@
+//
+//  ChartCell.swift
+//  Workout App
+//
+//  Created by Elliot Lee on 12/27/20.
+//
+
+import UIKit
+import Charts
+
+class ChartCell: UITableViewCell, ChartViewDelegate {
+
+    
+    var data = [(Date, Exercise)]()
+    
+    @IBOutlet weak var exercise_name_label: UILabel!
+    
+    var progress_chart = LineChartView()
+    
+    override func awakeFromNib() {
+        //print("did load cell")
+        super.awakeFromNib()
+        
+        self.contentView.backgroundColor = Constants.CELL_0()
+        exercise_name_label.textColor = Constants.TEXT()
+        progress_chart.delegate = self
+        
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        progress_chart.frame = CGRect(x: 0, y: 0 , width: self.contentView.frame.size.width - 10, height: self.contentView.frame.size.height - 60)
+        
+        progress_chart.center = CGPoint(x: contentView.center.x, y: contentView.center.y + 20 )
+        
+        progress_chart.rightAxis.enabled = false
+        progress_chart.legend.enabled = false
+        
+        progress_chart.xAxis.valueFormatter = ChartDateFormatter()
+        progress_chart.xAxis.granularity = 1
+        progress_chart.xAxis.drawGridLinesEnabled = false
+        progress_chart.xAxis.labelTextColor = Constants.TEXT()
+        progress_chart.xAxis.axisLineColor = Constants.TEXT()
+        progress_chart.xAxis.yOffset = 15
+        
+        progress_chart.leftAxis.labelTextColor = Constants.TEXT()
+        progress_chart.leftAxis.axisLineColor = Constants.TEXT()
+        progress_chart.leftAxis.drawGridLinesEnabled = false
+        progress_chart.leftAxis.xOffset = 15
+        
+        progress_chart.pinchZoomEnabled = true
+        progress_chart.doubleTapToZoomEnabled = false
+        
+        progress_chart.tintColor = Constants.BACKGROUND()
+        
+        contentView.addSubview(progress_chart)
+        
+        //set up data
+        var coordinates = [ChartDataEntry]()
+        //for i in 0..<10{
+        //let i = 4
+            //coordinates.append(ChartDataEntry(x: 1.0+Double(i),y: 1.0+Double(i) ))
+        //}
+        for coordinate in data{
+            let time_interval = coordinate.0.timeIntervalSince1970
+            let date_num = Double(time_interval)
+            
+            var volume = 0.0
+            for set in coordinate.1.sets{
+                if set.is_complete{
+                    volume += set.weight * Double(set.reps)
+                }
+            }
+            coordinates.insert(ChartDataEntry(x: date_num ,y: volume), at: 0)
+        }
+        
+        let set = LineChartDataSet(entries: coordinates)
+        set.mode = .cubicBezier
+        set.colors = [Constants.TEXT()]
+        set.circleColors = [Constants.SECTION()]
+        progress_chart.data = LineChartData(dataSet: set)
+        progress_chart.data?.setDrawValues(false)
+        
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
+
+class ChartDateFormatter : IAxisValueFormatter {
+    
+    func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+        
+        var index_1 = 0
+        var index_2 = 0
+        var index_3 = 0
+        let current_index = axis?.entries.firstIndex(of: value) ?? 0
+        let entry_count = (axis?.entries.count ?? 0) + 2
+        if entry_count > 4 {
+            if (entry_count) % 4 == 0 {
+                index_1 = entry_count / 4 - 1
+                index_2 = (entry_count / 4) * 2 - 1
+                index_3 = (entry_count / 4) * 3 - 1
+                
+            } else {
+                index_1 = (entry_count / 4)
+                index_2 = (entry_count / 4) * 2
+                index_3 = (entry_count / 4) * 3
+            }
+            
+            if current_index == 0 || current_index == index_1 || current_index == index_2 || current_index == index_3 {
+                let time_interval = TimeInterval(value)
+                let date = Date(timeIntervalSince1970: time_interval)
+                let month = Calendar.current.component(.month, from: date)
+                let day = Calendar.current.component(.day, from: date)
+                let year = Calendar.current.component(.year, from: date)
+                return "\(month)-\(day)-\(year)"
+            } else {
+                return ""
+            }
+
+        }
+                
+        let time_interval = TimeInterval(value)
+        let date = Date(timeIntervalSince1970: time_interval)
+        let month = Calendar.current.component(.month, from: date)
+        let day = Calendar.current.component(.day, from: date)
+        let year = Calendar.current.component(.year, from: date)
+        return "\(month)-\(day)-\(year)"
+    }
+}
--- Workout App/ProgressViewController.swift
+++ Workout App/ProgressViewController.swift
@@ -12,30 +12,93 @@ class ProgressViewController: UIViewController, UITableViewDelegate, UITableView
     
     
     var cycles = [Cycle]()
+    var chart_coords = [[(Date, Exercise)]]()
     var routine_delegate : RoutinesTableViewControllerDelegate?
     
     @IBOutlet weak var chart_table: UITableView!
     
     override func viewDidLoad() {
+        //print("loaded progress vc")
         super.viewDidLoad()
+        chart_table.delegate = self
+        chart_table.dataSource = self
         self.view.backgroundColor = Constants.BACKGROUND()
+        chart_table.backgroundColor = Constants.BACKGROUND()
         
+        chart_table.rowHeight = 300
+        create_coordinates()
+        //print("cycles that exist? \(cycles.count)")
+        //chart_table.reloadData()
     }
 
     func numberOfSections(in tableView: UITableView) -> Int {
-        <#code#>
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        <#code#>
+        return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return chart_coords.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "chart_cell", for: indexPath) as! ChartCell
+        
+        cell.exercise_name_label.text = chart_coords[indexPath.row][0].1.name
+        
+        cell.data = chart_coords[indexPath.row]
+        cell.backgroundColor = Constants.CELL_0()
+        //cell.textLabel?.text = "loading?"
+        return cell;
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func create_coordinates(){
+        var current_date = Date()
+        for cycle in cycles{
+            //print("cycle checked")
+            for day in cycle.days{
+                //print("    day checked")
+                current_date = day.date
+                for exercise in day.exercises{
+                    //print("          exercise checked")
+                    append_data(date: current_date, exercise: exercise)
+                }
+            }
+        }
+    }
+    
+    /*
+     this is a helper fuction to be used to append new exercises and dates to the dataset correctly
+     @param date of when the exercise was performed
+     @param exercise that was perormed
+     */
+    private func append_data(date : Date, exercise: Exercise){
+        
+        //if the data set is empty
+        if(chart_coords.count == 0){
+            var temp = [(Date, Exercise)]()
+            temp.append( (date, exercise) )
+            chart_coords.append(temp)
+            return;
+        }
+        
+        //loop through the data set and check if there is an existnig exercise array already
+        for i in 0..<chart_coords.count {
+            //print("looping to check")
+            if chart_coords[i][0].1.name.capitalized == exercise.name.capitalized{
+                chart_coords[i].append( (date, exercise) )
+                return;
+            }
+        }
+        
+        //if we didnt append to some array in the data set already
+        var temp = [(Date, Exercise)]()
+        temp.append( (date, exercise) )
+        chart_coords.append(temp)
+        return;
+        
     }
 
     /*
